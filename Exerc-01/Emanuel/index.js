2c7155d1698b6895e357fce9f75bb26455bca53a
emanuellMarinho <joaoemanuelribeiromarinho@gmail.com>
2024-09-21 14:01:26 -0300

    Exercício 01

---
+++ Exerc-01/Emanuel/index.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
+ * Area = Mult base * Altura / 2
+*/
+
+let areaTotal = 0
+function area(base, altura) {
+    return areaTotal = (base * altura) / 2
+}
+
+console.log(area(10, 10))
