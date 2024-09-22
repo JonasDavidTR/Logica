34130f364752968fa2766449afc8fac93cbb29fd
emanuellMarinho <joaoemanuelribeiromarinho@gmail.com>
2024-09-22 12:57:27 -0300

    Calcula o Perímetro de um Triângulo

---
+++ Exercicios/Exerc-4/Emanuel/index.js
@@ -0,0 +1,9 @@
+/**
+ * 4. Dado os três lados de um triângulo, determinar o perímetro do mesmo.
+ */
+
+function triangulo(a, b, c) {
+    return console.log(`${a + b + c}cm`)
+}
+
+triangulo(3, 4, 5)
