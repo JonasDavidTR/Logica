b64a241305921eb4a526cfd050443190918b0303
JonasDavidTR <jonasdavidcosta@gmail.com>
2024-10-02 11:23:21 -0300

    Solicitar a idade de uma pessoa em dias e informar na tela a idade em anos, meses e dias.

--- Exercicios/Exerc-7/Jonas/main.c
+++ Exercicios/Exerc-7/Jonas/main.c
@@ -4,9 +4,21 @@
 Solicitar a idade de uma pessoa em dias e informar na tela a idade em anos, meses e dias.
 */
 
-
 int main(){
 
+    int idadeEmDias, anos, meses, dias;
+
+    // Solicita a idade em dias
+    printf("Digite a idade em dias: ");
+    scanf("%d", &idadeEmDias);
+
+    // Calcula anos, meses e dias
+    anos = idadeEmDias / 365;
+    idadeEmDias %= 365;
+    meses = idadeEmDias / 30;
+    dias = idadeEmDias % 30;
+
+    printf("Idade: %d anos, %d meses, %d dias\n", anos, meses, dias);
 
     return 0;
 }
