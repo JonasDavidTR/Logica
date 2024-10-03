8c73daf583ed013c9e7378b1d784ba0952391255
emanuellMarinho <joaoemanuelribeiromarinho@gmail.com>
2024-10-03 07:40:41 -0300

    Exercicio 10 ajustado

--- Exercicios/Exerc-10/Emanuel/index.js
+++ Exercicios/Exerc-10/Emanuel/index.js
@@ -1,14 +1,34 @@
 // 10. Converter um inteiro menor que 32 para sua representação em binário.
 
-function inteiroParaBinario(num) {
-    let binario = ""
-    while (num > 0) {
-        let resto = num % 2
-        binario = resto + binario
-        num = (num - resto) / 2
+// function inteiroParaBinario(num) {
+//     let binario = ""
+//     while (num > 0) {
+//         let resto = num % 2
+//         binario = resto + binario
+//         num = (num - resto) / 2
+//     }
+//     return binario || "0"
+// }
+
+// let numero = 18
+// console.log(inteiroParaBinario(numero))
+
+function intForBin(num) {
+    let binario = new Array()
+    try {
+        while(num < 0) {
+            let resto = num & 2
+            binario.unshift(resto)
+            num = (num - resto) / 2
+        }
+    } catch (e) {
+        if(num >= 32 || num <= 0) {
+            console.log("Número inválido")
+            intForBin()
+        }
     }
-    return binario || "0"
+    return binario.length > 0 ? binario : [0]
 }
 
 let numero = 18
-console.log(inteiroParaBinario(numero))
+console.log(intForBin(numero))
